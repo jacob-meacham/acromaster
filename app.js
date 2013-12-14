1c92b3093424a932e1e2add17263718638d513f8
Jacob Meacham <jacob.e.meacham@gmail.com>
2013-12-14 13:17:35 -0800

    Break out server runner and app.

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+    fs = require('fs');
+
+var env = process.env.NODE_ENV || 'development',
+    config = require('./config/config')[env],
+    mongoose = require('mongoose');
+
+var db = mongoose.connect(config.db);
+
+// Add models
+var models_path = __dirname + '/server/models';
+fs.readdirSync(models_path).forEach(function(file) {
+    require(models_path + '/' + file);
+});
+
+var app = express();
+
+// Setup server
+require('./config/express')(app, config);
+
+// Hook up routes
+require('./server/routes/movelist')(app);
+
+//expose app
+exports = module.exports = app;
--- server.js
+++ server.js
@@ -1,32 +1,5 @@
-/**
- * Module dependencies.
- */
-var express = require('express'),
-    fs = require('fs');
-
-var env = process.env.NODE_ENV || 'development',
-    config = require('./config/config')[env],
-    mongoose = require('mongoose');
-
-var db = mongoose.connect(config.db);
-
-// Add models
-var models_path = __dirname + '/server/models';
-fs.readdirSync(models_path).forEach(function(file) {
-    require(models_path + '/' + file);
-});
-
-var app = express();
-
-// Setup server
-require('./config/express')(app, config);
-
-// Hook up routes
-require('./server/routes/movelist')(app);
+var app = require('./app');
 
 require('http').createServer(app).listen(app.get('port'), function () {
     console.log('Express (' + app.get('env') + ') server listening on port ' + app.get('port'));
 });
-
-//expose app
-exports = module.exports = app;
