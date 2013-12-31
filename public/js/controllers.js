672297a2a8e9659375aa7e23a3acf69679efad38
Jacob Meacham <jacob.e.meacham@gmail.com>
2013-12-30 21:31:12 -0800

    switch to latest versions of everything for now.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,6 @@
-'use strict'
+'use strict';
 
-var acromasterControllers = angular.module('acromaster.controllers', []);
-
+angular.module('acromaster.controllers', []);
 angular.module('acromaster.controllers').controller('IndexController', ['$scope', function($scope) {
 
 }]);
