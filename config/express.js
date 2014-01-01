37b1f566dbdb93e34cddde2340d23312575385cf
Jacob Meacham <jacob.e.meacham@gmail.com>
2013-12-31 18:26:03 -0800

    Fix deprecated warning.

--- config/express.js
+++ config/express.js
@@ -25,7 +25,8 @@ module.exports = function(app, config) {
     app.enable('jsonp callback');
     
     app.use(express.cookieParser());
-    app.use(express.bodyParser());
+    app.use(express.json());
+    app.use(express.urlencoded());
     app.use(express.methodOverride());
 
     // express/mongo session storage
