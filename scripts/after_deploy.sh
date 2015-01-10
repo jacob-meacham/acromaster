01a65f6423aa082fffa3dee8b2d935b8dfd543bc
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-01-10 13:41:32 -0800

    Ignore key warnings.

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -18,10 +18,8 @@ echo "Host heroku.com" >> ~/.ssh/config
 echo " StrictHostKeyChecking no" >> ~/.ssh/config
 echo " CheckHostIP no" >> ~/.ssh/config
 echo " UserKnownHostsFile=/dev/null" >> ~/.ssh/config
-heroku keys:clear --app $app_name
-yes | heroku keys:add --app $app_name
 
 hash_name=COMMIT_HASH
 hash=$(git rev-parse HEAD | cut -c-10)
 echo Setting $hash_name to $hash
-yes | heroku config:set $hash_name=$hash --app $app_name
+heroku config:set $hash_name=$hash --app $app_name
