71fabad55e2d3f9f5707be964a84233dad5658d5
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-01-10 14:38:37 -0800

    I'm not good at bash :(

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
-if [ $TRAVIS_BRANCH == "develop" ]
-    then
-        app_name=acromaster-staging
-    else
-        app_name=acromaster
+if [ "$TRAVIS_BRANCH" = "develop" ]; then
+    app_name=acromaster-staging
+else
+    app_name=acromaster
 fi
 echo Deploying to app $app_name...
 
-if [ $TRAVIS_TAG ]
-    then
-        verion=$TRAVIS_TAG-$TRAVIS_COMMIT
-    else
-        version=$TRAVIS_COMMIT
+if [ $TRAVIS_TAG ]; then
+    verion=$TRAVIS_TAG-$TRAVIS_COMMIT
+else
+    version=$TRAVIS_COMMIT
 fi
 
 version_name=VERSION
 echo Setting $version_name to $version
-curl -s -w "%{http_code}" -n -o /dev/null -X PATCH https://api.heroku.com/apps/acromaster-staging/config-vars \
+curl -s -w "%{http_code}" -n -o /dev/null -X PATCH https://api.heroku.com/apps/$app_name/config-vars \
 -H "Accept: application/vnd.heroku+json; version=3" \
 -H "Authorization: Bearer $API_KEY" \
 -H "Content-Type: application/json" \
