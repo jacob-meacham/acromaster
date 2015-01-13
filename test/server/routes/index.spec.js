2126dfc72ddaaa2cb20bfb3c05493fe901c03eba
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-01-12 22:41:23 -0800

    Add test coverage to main routes

---
+++ test/server/routes/index.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var request = require('supertest');
+var app = require('../../../server');
+
+describe('GET /version', function() {
+  it('Should return a version string', function(done) {
+    request(app)
+      .get('/version')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(/\b[0-9a-f]{5,40}\b|Development version/, done);
+  });
+});
+
+describe('GET /', function() {
+  it('Should return an HTML page', function(done) {
+    request(app)
+      .get('/')
+      .expect('Content-Type', /text\/html/)
+      .expect(200, done);
+  });
+});
