c433c6589720209c4fbebef3424b5f2ed68a4b9a
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-01-31 13:31:44 -0800

    Remove console.logs

--- server/config/passport.js
+++ server/config/passport.js
@@ -20,18 +20,14 @@ var userCallback = function(profile, idProp, userCreator, done) {
     query[idProp] = profile.id;
     User.findOne(query, function(err, user) {
         if (err) {
-            console.log('Trying to find user resulted in error ' + err);
             return done(err);
         }
         if (!user) {
             user = userCreator(profile);
-            console.log('created user ' + user);
             user.save(function(err) {
-                console.log('User saved: ' + user + ', ' + err);
                 return done(err, user);
             });
         } else {
-            console.log('User already exists yo ' + user + ', ' + err);
             return done(err, user);
         }
     });
@@ -50,7 +46,6 @@ var twitterCallback = function(token, tokenSecret, profile, done) {
 };
 
 var facebookCallback = function(accessToken, refreshToken, profile, done) {
-    console.log('facebook callback: ' + accessToken + ', ' + refreshToken + ', ' + profile);
     var userCreator = function(profile) {
         return new User({
             name: profile.displayName,
