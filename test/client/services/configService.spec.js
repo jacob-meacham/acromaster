01e514bc834309f4b84b3a4b20b785814175508a
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-01-31 21:45:57 -0800

    Add tests for configService

--- test/client/services/configService.spec.js
+++ test/client/services/configService.spec.js
@@ -1,8 +1,40 @@
 'use strict';
 
-describe('configService', function() {
-  //beforeEach(module('acromaster.services'));
-  it('should pass', function(done) {
-    done();
+describe('ConfigService', function() {
+  beforeEach(module('acromaster'));
+  var $window;
+
+  beforeEach(inject(function(_$window_) {
+    $window = _$window_;
+  }));
+
+  describe('_ factory', function() {
+      var _;
+      beforeEach(inject(function(___) {
+        _ = ___;
+      }));
+
+      it('should return lodash from window', function() {
+        _.should.equal($window._);
+      });
+  });
+
+  describe('environment factory', function() {
+    var environment;
+    beforeEach(inject(function(_environment_) {
+      environment = _environment_;
+    }));
+
+    describe('isDebug', function() {
+      it('should return true when env is development', function() {
+        $window.env = 'development';
+        environment.isDebug().should.be.true();
+      });
+
+      it('should return false when env is not development', function() {
+        $window.env = null;
+        environment.isDebug().should.not.be.true();
+      });
+    });
   });
 });
