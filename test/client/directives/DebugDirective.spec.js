cb1ba7aa724aa09a67071d29b6faacc82497626c
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-02-03 23:09:46 -0800

    Test DebugDirective

---
+++ test/client/directives/DebugDirective.spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+describe('DebugDirective', function() {
+  beforeEach(module('acromaster'));
+
+  var $compile;
+  var $rootScope;
+  var environment;
+
+  beforeEach(function() {
+    // Stub-stub, so that sinon can actually stub isDebug later on
+    environment = {
+      isDebug: function() {}
+    };
+
+    module(function ($provide) {
+        $provide.value('environment', environment);
+    });
+
+    inject(function(_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+    });
+  });
+
+  it('should include ng-hide when debug is false', function() {
+    sinon.stub(environment, 'isDebug').returns(false);
+    var element = $compile('<debug>f00</debug>')($rootScope);
+    element.hasClass('ng-hide').should.be.true();
+  });
+
+  it('should not include ng-hide when debug is true', function() {
+    sinon.stub(environment, 'isDebug').returns(true);
+    var element = $compile('<debug>f00</debug>')($rootScope);
+    element.hasClass('ng-hide').should.be.false();
+  });
+});
