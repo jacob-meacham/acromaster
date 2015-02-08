6c32b939a3e85efd21ef553b67ab00f87beb92d9
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-02-07 18:42:37 -0800

    Split out most of the flow editing into a directive. Test Crud

--- public/js/controllers/FlowCrudController.js
+++ public/js/controllers/FlowCrudController.js
@@ -12,151 +12,19 @@ controllers.controller('FlowListController', ['$scope', 'Flow', function($scope,
   find();
 }]);
 
-controllers.controller('FlowCreateController', ['$scope', '$location', 'flash', '_', 'Flow', 'Moves', function($scope, $location, flash, _, Flow, Moves) {
-  $scope.allMoves = Moves.query();
-  var flow = $scope.flow = new Flow({moves: []});
-
-  $scope.moveList = [];
-
-  // TODO: Refactor into directive.
-  $scope.addMove = function() {
-    $scope.inserted = {
-      move: null,
-      duration: 20
-    };
-    
-    $scope.moveList.push($scope.inserted);
-  };
-
-  $scope.updateMove = function(index, $data) {
-    $scope.moveList[index].move = $data;
-  };
-
-  $scope.removeMove = function(index) {
-    $scope.moveList.splice(index, 1);
-  };
-
-  $scope.cancelMove = function(index) {
-    // If the move has no name, just remove.
-    $scope.moveList.splice(index, 1);
-  };
-
-  $scope.checkMove = function($data) {
-    console.log($data);
-    if ($data === undefined) {
-      return 'No move specified';
-    }
-
-    if (_.indexOf($scope.allMoves, $data) < 0) {
-      return 'Not a valid move';
-    }
-  };
-
-  $scope.checkDuration = function($data) {
-    if ($data === undefined) {
-      return 'Move must have a duration';
-    }
-
-    var duration = parseInt($data, 10);
-    if (isNaN(duration) || duration < 0) {
-      return 'Duration must be a valid number';
-    }
-  };
-
-  $scope.create = function() {
-    flow.moves = [];
-    for (var i = 0; i < $scope.moveList.length; i++) {
-      flow.moves.push({
-        move: $scope.moveList[i].move._id,
-        duration: $scope.moveList[i].duration
-      });
-    }
-
-    var saveSuccess = function(savedFlow) {
-      $location.path('/flow/' + savedFlow._id);
-    };
-    var saveFailure = function() {
-      flash.error = 'There was an issue saving your flow. Correct any issues and re-submit';
-    };
-
-    flow.$save(saveSuccess, saveFailure);
+controllers.controller('FlowCreateController', ['$scope', '$location', 'Flow', function($scope, $location, Flow) {
+  $scope.flow = new Flow({moves: []});
+  
+  $scope.saveSuccess = function(savedFlow) {
+    $location.path('/flow/' + savedFlow._id);
   };
 }]);
 
-controllers.controller('FlowEditController', ['$scope', '$routeParams', '$location', 'flash', '_', 'Flow', 'Moves', function($scope, $routeParams, $location, flash, _, Flow, Moves) {
-  $scope.allMoves = Moves.query();
-  $scope.moveList = [];
-  var flow = $scope.flow = Flow.get({flowId: $routeParams.flowId}, function() {
-    angular.forEach(flow.moves, function(entry) {
-      $scope.moveList.push({
-        move: entry.move,
-        duration: entry.duration
-      });
-    });
-  });
+controllers.controller('FlowEditController', ['$scope', '$routeParams', '$location', 'FlowService', function($scope, $routeParams, $location, FlowService) {
+  $scope.flow = FlowService.instantiateFlow($routeParams.flowId);
 
-  // TODO: Refactor into directive.
-  $scope.addMove = function() {
-    $scope.inserted = {
-      move: null,
-      duration: 20
-    };
-    
-    $scope.moveList.push($scope.inserted);
-  };
-
-  $scope.updateMove = function(index, $data) {
-    $scope.moveList[index].move = $data;
-  };
-
-  $scope.removeMove = function(index) {
-    $scope.moveList.splice(index, 1);
-  };
-
-  $scope.cancelMove = function(index) {
-    // If the move has no name, just remove.
-    $scope.moveList.splice(index, 1);
-  };
-
-  $scope.checkMove = function($data) {
-    console.log($data);
-    if ($data === undefined) {
-      return 'No move specified';
-    }
-
-    if (_.indexOf($scope.allMoves, $data) < 0) {
-      return 'Not a valid move';
-    }
-  };
-
-  $scope.checkDuration = function($data) {
-    if ($data === undefined) {
-      return 'Move must have a duration';
-    }
-
-    var duration = parseInt($data, 10);
-    if (isNaN(duration) || duration < 0) {
-      return 'Duration must be a valid number';
-    }
-  };
-
-  $scope.create = function() {
-    flow.moves = [];
-    for (var i = 0; i < $scope.moveList.length; i++) {
-      flow.moves.push({
-        move: $scope.moveList[i].move._id,
-        duration: $scope.moveList[i].duration
-      });
-    }
-
-    var saveSuccess = function(savedFlow) {
-      $location.path('/flow/' + savedFlow._id);
-    };
-    var saveFailure = function() {
-      flash.error = 'There was an issue saving your flow. Correct any issues and re-submit';
-    };
-
-    flow.$update(flow._id, saveSuccess, saveFailure);
+  $scope.saveSuccess = function(savedFlow) {
+    $location.path('/flow/' + savedFlow._id);
   };
 }]);
 
---
+++ public/js/directives/FlowEditDirective.js
@@ -0,0 +1,99 @@
+'use strict';
+
+angular.module('acromaster.directives').directive('floweditor', function() {
+  var controller = ['_', 'Moves', 'flash', function(_, Moves, flash) {
+    var vm = this;
+
+    vm.allMoves = Moves.query();
+    vm.moveList = [];
+
+    if (vm.flow.$promise) {
+      // TODO: :(
+      // Wait for the promise to be fulfilled before instantiating the move list
+      vm.flow.$promise.then(function() {
+        angular.forEach(vm.flow.moves, function(entry) {
+          vm.moveList.push({
+            move: entry.move,
+            duration: entry.duration
+          });
+        });
+      });
+    }
+
+    vm.addMove = function() {
+      vm.inserted = {
+        move: null,
+        duration: 20
+      };
+      
+      vm.moveList.push(vm.inserted);
+    };
+
+    vm.updateMove = function(index, $data) {
+      vm.moveList[index].move = $data;
+    };
+
+    vm.removeMove = function(index) {
+      vm.moveList.splice(index, 1);
+    };
+
+    vm.cancelMove = function(index) {
+      // If the move has no name, just remove.
+      vm.moveList.splice(index, 1);
+    };
+
+    vm.checkMove = function($data) {
+      if (!$data) {
+        return 'No move specified';
+      }
+
+      if (_.indexOf(vm.allMoves, $data) < 0) {
+        return 'Not a valid move';
+      }
+    };
+
+    vm.checkDuration = function($data) {
+      if (!$data) {
+        return 'Move must have a duration';
+      }
+
+      var duration = parseInt($data, 10);
+      if (isNaN(duration) || duration < 0) {
+        return 'Duration must be a valid number';
+      }
+    };
+
+    vm.save = function() {
+      vm.flow.moves = [];
+      angular.forEach(vm.moveList, function(entry) {
+        vm.flow.moves.push({
+          move: entry.move._id,
+          duration: entry.duration
+        });
+      });
+      
+      var saveFailure = function() {
+        flash.error = 'There was an issue saving your flow. Correct any issues and re-submit';
+      };
+
+      if (vm.flow._id) {
+        // Update an existing flow
+        vm.flow.$update(vm.saveSuccess, saveFailure);
+      } else {
+        vm.flow.$save(vm.saveSuccess, saveFailure);
+      }
+    };
+  }];
+
+  return {
+    restrict: 'E',
+    scope: {
+      flow: '=',
+      saveSuccess: '&onSaveSuccess'
+    },
+    templateUrl: '../../../partials/flow/floweditor.html',
+    controller: controller,
+    controllerAs: 'vm',
+    bindToController: true
+  };
+});
