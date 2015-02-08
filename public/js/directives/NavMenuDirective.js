e669e6a1434751da18eda335fb5c1fadf9ca2dd4
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-02-08 12:35:18 -0800

    Test NavMenu

--- public/js/directives/NavMenuDirective.js
+++ public/js/directives/NavMenuDirective.js
@@ -3,38 +3,37 @@
 var directives = angular.module('acromaster.directives');
 
 directives.directive('navMenu', ['$location', function($location) {
-  return function(scope, element, attrs) {
-    var links = element.find('a'),
-        elements = element.find('li'),
-        activeClass = attrs.navMenu || 'active',
-        link,
-        url,
-        currentLink,
-        urlMap = {},
-        i;
+  return {
+    link: function(scope, element, attrs) {
+      var links = element.find('a');
+      var elements = element.find('li');
+      var activeClass = attrs.navMenu || 'active';
+      var currentLink;
+      var urlMap = {};
 
-    var setActiveLink = function() {
-      var pathLink = urlMap[$location.path()];
+      var setActiveLink = function() {
+        var pathLink = urlMap[$location.path()];
 
-      if (pathLink) {
-        if (currentLink) {
-          currentLink.removeClass(activeClass);
+        if (pathLink) {
+          if (currentLink) {
+            currentLink.removeClass(activeClass);
+          }
+          currentLink = pathLink;
+          currentLink.addClass(activeClass);
         }
-        currentLink = pathLink;
-        currentLink.addClass(activeClass);
+      };
+
+      for (var i = 0; i < links.length; i++) {
+        var link = angular.element(links[i]);
+        var url = link.attr('href');
+        urlMap[url] = angular.element(elements[i]);
       }
-    };
 
-    for (i = 0; i < links.length; i++) {
-      link = angular.element(links[i]);
-      url = link.attr('href');
-      urlMap[url] = angular.element(elements[i]);
-    }
-
-    setActiveLink();
-
-    scope.$on('$routeChangeStart', function() {
       setActiveLink();
-    });
+
+      scope.$on('$routeChangeStart', function() {
+        setActiveLink();
+      });
+    }
   };
 }]);
