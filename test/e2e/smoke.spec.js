71f99b51c3dfd0fc49454fb35545ffd39e9c21c2
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-02-14 23:27:19 -0800

    Remove unneeded block

--- test/e2e/smoke.spec.js
+++ test/e2e/smoke.spec.js
@@ -7,10 +7,6 @@ chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 describe('smoke test', function() {
-  beforeEach(function() {
-    this.slow(6000);
-  });
-
   it('should load', function(done) {
     browser.get('/');
     expect(browser.getTitle()).to.eventually.equal('Acromaster - Test').notify(done);
