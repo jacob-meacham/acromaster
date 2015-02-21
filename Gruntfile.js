df8d6297f91d646a85f4146ffae695941aeb88f5
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-02-21 10:57:41 -1000

    Fix order of open and watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -261,7 +261,7 @@ module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
 
     grunt.registerTask('lint', ['jshint', 'csslint']);
-    grunt.registerTask('default', ['lint', 'env:dev', 'karma:dev', 'express:dev', 'watch', 'open:dev']);
+    grunt.registerTask('default', ['lint', 'env:dev', 'karma:dev', 'express:dev', 'open:dev', 'watch']);
     grunt.registerTask('protractor', ['env:test', 'selenium_start', 'mongoimport', 'express:ci', 'mochaProtractor']);
     grunt.registerTask('test', ['lint', 'env:test', 'mongodrop', 'mocha_istanbul:coverage', 'karma:ci', 'protractor', 'lcovMerge']);
     grunt.registerTask('heroku:production', ['cssmin:production', 'uglify:production']);
