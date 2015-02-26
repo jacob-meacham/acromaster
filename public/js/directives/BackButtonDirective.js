7e495d5946b547a4c97e6d768a6894579ca04989
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-02-25 23:47:54 -0800

    Hook up back button directive.

--- public/js/directives/BackButtonDirective.js
+++ public/js/directives/BackButtonDirective.js
@@ -5,7 +5,7 @@ var directives = angular.module('acromaster.directives');
 directives.directive('backButton', [function() {
   return {
     restrict: 'E',
-    template: '<a ng-href="/"><span class="glyphicon glyphicon-circle-arrow-left large-glyphicon back-button"></span></a>',
+    template: '<a ng-href="{{url}}"><span class="glyphicon glyphicon-circle-arrow-left large-glyphicon back-button"></span></a>',
     scope: {
       'url': '@'
     }
