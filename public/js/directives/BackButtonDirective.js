bf81e940d510527412da291ae693fea4bf3de6f5
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-02-25 23:43:24 -0800

    Add directive for the back button.

---
+++ public/js/directives/BackButtonDirective.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var directives = angular.module('acromaster.directives');
+
+directives.directive('backButton', [function() {
+  return {
+    restrict: 'E',
+    template: '<a ng-href="/"><span class="glyphicon glyphicon-circle-arrow-left large-glyphicon back-button"></span></a>',
+    scope: {
+      'url': '@'
+    }
+  };
+}]);
