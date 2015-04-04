23263b90ab35ac2b2536e2e118fb1eda4b6a1552
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-04-04 13:14:14 +0200

    export schemas from models, move move route to its own file.

---
+++ server/routes/moves.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var Move = require('../models/move.js');
+
+var getMoves = function(req, res, next) {
+  // TODO: Don't allow arbitrary input
+  var query = {};
+  if (req.query !== null) {
+    query = req.query;
+  }
+  
+  Move.find(query, function(err, moves) {
+    if (err) {
+      return next(err);
+    }
+
+    res.jsonp(moves);
+  });
+};
+
+module.exports = function(app) {
+  app.get('/api/moves', getMoves);
+};
