e92186224638ff4f24016e6fd652b5fd7aa4f9fa
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-05-11 20:21:14 -0700

    Fix fallout for rename of PageHeaderService.

--- public/js/controllers/FlowCrudController.js
+++ public/js/controllers/FlowCrudController.js
@@ -2,13 +2,13 @@
 
 var controllers = angular.module('acromaster.controllers');
 
-controllers.controller('FlowHomeController', ['$scope', '$location', 'Flow', 'HeadService', function($scope, $location, Flow, HeadService) {
+controllers.controller('FlowHomeController', ['$scope', '$location', 'Flow', 'PageHeaderService', function($scope, $location, Flow, PageHeaderService) {
   Flow.get({random: true, max: 11}, function(response) {
     $scope.randomFlow = response.flows[0];
     $scope.featuredFlows = response.flows.slice(1, response.total);
   });
 
-  HeadService.title = 'Acromaster - Flows';
+  PageHeaderService.setTitle('Acromaster - Flows');
 
   $scope.search = function() {
     $location.path('/flows/results').search({search_query: $scope.searchQuery});
