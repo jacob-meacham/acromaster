e718d19764e4f15deab0422e3a8f142b75e25a6d
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-05-14 23:10:27 -0800

    Add some profile tests.

--- test/client/controllers/ProfileController.spec.js
+++ test/client/controllers/ProfileController.spec.js
@@ -35,4 +35,38 @@ describe('Profile*Controllers', function() {
       expect(true).to.be.true;
     });
   });
+
+  describe('ProfileStatsController', function() {
+    it('should set the profile from the server', function() {
+      var profile = {name: 'foo', flows: ['a', 'b']};
+      sandbox.stub(User, 'get').returns(profile);
+      var $scope = {};
+      
+      // Create controller
+      $controller('ProfileHomeController', { $scope: $scope });
+      $scope.profile.should.eql(profile);
+    });
+
+    it('should show an error if the user does not exist', function() {
+      // TODO
+      expect(true).to.be.true;
+    });
+  });
+
+  describe('ProfileAchievementsController', function() {
+    it('should set the profile from the server', function() {
+      var profile = {name: 'foo', flows: ['a', 'b']};
+      sandbox.stub(User, 'get').returns(profile);
+      var $scope = {};
+      
+      // Create controller
+      $controller('ProfileHomeController', { $scope: $scope });
+      $scope.profile.should.eql(profile);
+    });
+
+    it('should show an error if the user does not exist', function() {
+      // TODO
+      expect(true).to.be.true;
+    });
+  });
 });
