cb32a353478a7817d09a14a90132a1c72bb373f8
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-05-17 12:06:30 -0700

    Change how the controllers are created.

--- public/js/controllers/ProfileController.js
+++ public/js/controllers/ProfileController.js
@@ -1,34 +1,40 @@
 'use strict';
 
-var acromasterControllers = angular.module('acromaster.controllers');
-
-acromasterControllers.controller('ProfileHomeController', ['$scope', '$routeParams', 'User', function($scope, $routeParams, User) {
-  $scope.profile = User.get({userId: $routeParams.user});
-}]);
-
-acromasterControllers.controller('ProfileStatsController', ['$scope', '$routeParams', 'User', function($scope, $routeParams, User) {
-  $scope.profile = User.get({userId: $routeParams.user});
-}]);
-
 var setupPagination = function($scope, $http, type, userid) {
   // TODO: Into a service we go!
   $scope.flows = $http.get('/api/profile/' + userid + '/' + type);
   $scope.perPage = 25;
 };
 
-acromasterControllers.controller('ProfileFlowsController', ['$scope', '$routeParams', 'User', '$http', function($scope, $routeParams, User, $http) {
+var ProfileHomeController = function($scope, $routeParams, User) {
+  $scope.profile = User.get({userId: $routeParams.user});
+};
+
+var ProfileStatsController = function($scope, $routeParams, User) {
+  $scope.profile = User.get({userId: $routeParams.user});
+};
+
+var ProfileFlowsController = function($scope, $routeParams, User, $http) {
   $scope.profile = User.get({userId: $routeParams.user});
 
   setupPagination($scope, $http, 'flows', $routeParams.user);
-  
-}]);
+};
 
-acromasterControllers.controller('ProfileFavoritesController', ['$scope', '$routeParams', 'User', '$http', function($scope, $routeParams, User, $http) {
+var ProfileFavoritesController = function($scope, $routeParams, User, $http) {
   $scope.profile = User.get({userId: $routeParams.user});
 
   setupPagination($scope, $http, 'favorites', $routeParams.user);
-}]);
+};
 
-acromasterControllers.controller('ProfileAchievementsController', ['$scope', '$routeParams', 'User', function($scope, $routeParams, User) {
+acromasterControllers.controller('ProfileAchievementsController', ['$scope', '$routeParams', 'User', ProfileAchievementsController]);
+
+var ProfileAchievementsController = function($scope, $routeParams, User) {
   $scope.profile = User.get({userId: $routeParams.user});
-}]);
+};
+
+var acromasterControllers = angular.module('acromaster.controllers');
+acromasterControllers.controller('ProfileHomeController', ['$scope', '$routeParams', 'User', ProfileHomeController]);
+acromasterControllers.controller('ProfileStatsController', ['$scope', '$routeParams', 'User', ProfileStatsController]);
+acromasterControllers.controller('ProfileFlowsController', ['$scope', '$routeParams', 'User', '$http', ProfileFlowsController]);
+acromasterControllers.controller('ProfileFavoritesController', ['$scope', '$routeParams', 'User', '$http', ProfileFavoritesController]);
+acromasterControllers.controller('ProfileAchievementsController', ['$scope', '$routeParams', 'User', ProfileAchievementsController]);
