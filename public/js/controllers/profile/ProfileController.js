f04f9c64a3b2ff540caa9f49a6d357b445e386b7
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-05-31 22:00:17 -0700

    Add stats styling to the profile page.

--- public/js/controllers/profile/ProfileController.js
+++ public/js/controllers/profile/ProfileController.js
@@ -1,9 +1,71 @@
 'use strict';
 
-var ProfileHomeController = function($routeParams, User, pageHeaderService) {
+var ProfileHomeController = function($routeParams, $timeout, $window, rand, User, pageHeaderService, _) {
   var vm = this;
-  vm.profile = User.get({userId: $routeParams.user});
   pageHeaderService.setTitle($routeParams.user);
+
+  var commonOptions = {
+    value: 0,
+    gaugeWidthScale: 0.2,
+    donut: true,
+    relativeGaugeSize: true,
+    valueFontColor: '#fff',
+    titleFontColor: '#fff',
+    gaugeColor: '#00000000',
+    counter: false,
+    donutStartAngle: 270,
+    title: ''
+  };
+
+  vm.minutesPlayedOptions = {};
+  vm.movesPlayedOptions = {};
+
+  vm.flowsPlayedOptions = _.merge({
+    min: 0,
+    max: 100,
+    levelColors: ['#CE1B21'],
+    textRenderer: function() {
+      return '\uf0e7';
+    }
+  }, commonOptions);
+
+  vm.minutesPlayedOptions = _.merge({
+    min: 0,
+    max: 2000,
+    levelColors: ['#00FF00'],
+    textRenderer: function() {
+      return '\uf017';
+    }
+  }, commonOptions);
+
+  vm.movesPlayedOptions = _.merge({
+    min: 0,
+    max: 1000,
+    levelColors: ['#FFFF00'],
+    textRenderer: function() {
+      return '\uf06d';
+    }
+  }, commonOptions);
+
+  vm.profile = User.get({userId: $routeParams.user});
+  vm.profile.$promise.then(function() {
+    var stats = vm.profile.stats;
+    vm.flowsPlayedOptions.max = $window.Math.max(vm.flowsPlayedOptions.max, stats.flowsPlayed / (1.5 + rand.random()));
+    vm.minutesPlayedOptions.max = $window.Math.max(vm.minutesPlayedOptions.max, stats.minutesPlayed / (1.5 + rand.random()));
+    vm.movesPlayedOptions.max = $window.Math.max(vm.movesPlayedOptions.max, stats.moves / (1.5 + rand.random()));
+
+    $timeout(function() {
+      vm.flowsPlayedOptions.value = stats.flowsPlayed;
+    }, 800);
+
+    $timeout(function() {
+      vm.minutesPlayedOptions.value = stats.minutesPlayed;
+    }, 1000);
+
+    $timeout(function() {
+      vm.movesPlayedOptions.value = stats.moves;
+    }, 1200);
+  });
 };
 
 var ProfileStatsController = function($routeParams, User, pageHeaderService) {
@@ -35,7 +97,7 @@ var ProfileAchievementsController = function($routeParams, User, pageHeaderServi
 };
 
 angular.module('acromaster.controllers')
-  .controller('ProfileHomeController', ['$routeParams', 'User', 'PageHeaderService', ProfileHomeController])
+  .controller('ProfileHomeController', ['$routeParams', '$timeout', '$window', 'RandomService', 'User', 'PageHeaderService', '_', ProfileHomeController])
   .controller('ProfileStatsController', ['$routeParams', 'User', 'PageHeaderService', ProfileStatsController])
   .controller('ProfileFlowsController', ['$routeParams', 'User', 'PageHeaderService', ProfileFlowsController])
   .controller('ProfileFavoritesController', ['$routeParams', 'User', 'PageHeaderService', ProfileFavoritesController])
