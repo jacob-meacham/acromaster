26641cc8510dda8eb2a6b51138bb0233de40f32a
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-05-31 21:59:51 -0700

    Make debug directive more complex, nicer for showing.

--- public/js/directives/DebugDirective.js
+++ public/js/directives/DebugDirective.js
@@ -3,10 +3,29 @@
 var debug = function(environment) {
   return {
     restrict: 'E',
+    transclude: true,
+    template: '<div class="debug"><button class="btn btn-default" ng-click="toggleCollapse()">{{label}}</button><pre class="debug-inner" collapse="isCollapsed"><ng-transclude></ng-transclude></pre></div>',
     link: function(scope, element) {
       if (!environment.isDebug()) {
         element.addClass('ng-hide');
       }
+
+      scope.isCollapsed = true;
+
+      var setLabel = function() {
+        if (scope.isCollapsed) {
+          scope.label = 'Show Debug Info...';
+        } else {
+          scope.label = 'Hide Debug Info...';
+        }
+      };
+
+      scope.toggleCollapse = function() {
+        scope.isCollapsed = !scope.isCollapsed;
+        setLabel();
+      };
+
+      setLabel();
     }
   };
 };
