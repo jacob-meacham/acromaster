b2803a1597dc5ee8f890e32f94b47b1928d71e66
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-07-16 16:30:08 -0700

    Add ability to share washing machines.

--- public/app/washing-machine/WashingMachineController.js
+++ public/app/washing-machine/WashingMachineController.js
@@ -1,20 +1,40 @@
 'use strict';
 
-var WaschingMachineViewController = function(WashingMachineService, pageHeaderService) {
+var WaschingMachineViewController = function($routeParams, WashingMachineService, pageHeaderService) {
   var vm = this;
 
   pageHeaderService.setTitle('Washing Machines');
   
   vm.generate = function() {
     WashingMachineService.generate().then(function(washingMachine) {
-      vm.move1 = washingMachine.move1;
-      vm.move2 = washingMachine.move2;
+      vm.move1 = washingMachine.move1.name;
+      vm.move2 = washingMachine.move2.name;
       vm.washing_machine = washingMachine.name;
+
+      // TODO: Make coords more concise?
+      vm.coords = [washingMachine.move1.name, washingMachine.move2.name, washingMachine.name].join(',');
     });
   };
 
-  vm.generate();
+  var needGeneration = true;
+  if ($routeParams.coords) {
+    try {
+      needGeneration = false;
+      
+      vm.coords = $routeParams.coords;
+      var coords = vm.coords.split(',');
+      vm.move1 = coords[0];
+      vm.move2 = coords[1];
+      vm.washing_machine = coords[2];
+    } catch (err) {
+      needGeneration = true;
+    }
+  }
+
+  if (needGeneration) {
+    vm.generate();
+  }
 };
 
 angular.module('acromaster.controllers')
-  .controller('WashingMachineViewController', ['WashingMachineService', 'PageHeaderService', WaschingMachineViewController]);
+  .controller('WashingMachineViewController', ['$routeParams', 'WashingMachineService', 'PageHeaderService', WaschingMachineViewController]);
