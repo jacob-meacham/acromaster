7207330f8122908e53cbe3349bb7a9435614d558
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-09-08 22:18:09 -0700

    Fix error interceptor to be more forgiving.

--- public/app/common/HttpErrorsInterceptor.js
+++ public/app/common/HttpErrorsInterceptor.js
@@ -4,8 +4,10 @@ var httpErrorsInterceptor = function($q, $log, flash) {
   return {
     'responseError': function(response) {
       $log.log(response);
-      if (response.data.error) {
+      if (response.data && response.data.error) {
         flash.error = response.data.error;
+      } else if (response.status && response.status === 0) {
+        flash.error = 'Your request timed out. Please check your connection and try again.';
       } else {
         flash.error = 'An internal server error occurred. Please contact feedback@acromaster.com.';
       }
