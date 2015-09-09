067b423ab24afa6eb08b472100c688fd728b5f5f
Jacob Meacham <jacob.e.meacham@gmail.com>
2015-09-09 13:34:06 -0700

    Initial code to reorder.

--- public/app/flow/edit/FlowEditDirective.js
+++ public/app/flow/edit/FlowEditDirective.js
@@ -59,6 +59,40 @@ var FlowEditDirectiveController = function($scope, _, Moves, flash, randomServic
     }
   };
 
+  vm.canMoveUp = function(index) {
+    return index >= 1;
+  };
+
+  vm.moveUp = function(index) {
+    if (!vm.canMoveUp(index)) {
+      console.log('cant move up');
+      return;
+    }
+
+    console.log('swapping up from ' + index + ' to ' + (index-1));
+
+    var swappedMove = vm.moveList[index-1];
+    vm.moveList[index-1] = vm.moveList[index];
+    vm.moveList[index] = swappedMove;
+  };
+
+  vm.canMoveDown = function(index) {
+    return index < vm.moveList.length-1;
+  };
+
+  vm.moveDown = function(index) {
+    if (!vm.canMoveDown(index)) {
+      console.log('cant move down');
+      return;
+    }
+
+    console.log('swapping down from ' + index + ' to ' + (index+1));
+
+    var swappedMove = vm.moveList[index+1];
+    vm.moveList[index+1] = vm.moveList[index];
+    vm.moveList[index] = swappedMove;
+  };
+
   vm.randomMove = function() {
     var moveEntry = {
       move: randomService.choose(vm.allMoves),
